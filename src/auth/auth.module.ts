2fe32e778592d79e412ed16bcafcdad69f2ec8da
gavryk <gvozdyk23@gmail.com>
2023-11-07 04:15:08 +0200

    add: signup functionality

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+	controllers: [AuthController],
+	providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-  signin() {
-    return this.prisma.user.findMany();
-  }
+	constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return 'I Am Signup';
-  }
+	async signup(dto: AuthDto) {
+		//generate the password hash
+		const hash = await argon.hash(dto.password);
+		//save the new user in the db
+		const user = await this.prisma.user.create({
+			data: {
+				email: dto.email,
+				name: dto.name,
+				hash,
+			},
+		});
+		delete user.hash;
+		//return the saved user
+		return user;
+	}
+
+	signin() {
+		return this.prisma.user.findMany();
+	}
 }
