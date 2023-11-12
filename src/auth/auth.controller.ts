d454e5986ea01703f45ab8aafc9a9089c013309a
gavryk <gvozdyk23@gmail.com>
2023-11-13 01:26:53 +0200

    fix: set token in cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res, UnauthorizedException } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Response } from 'express';
@@ -7,6 +7,7 @@ import { Response } from 'express';
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
+	@HttpCode(HttpStatus.OK)
 	@Post('signup')
 	signup(@Body() dto: AuthDto) {
 		return this.authService.signup(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { GetUser } from '../decorators';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
+	constructor(private prisma: PrismaService) {}
+
 	@UseGuards(JwtGuard)
 	@Get('me')
-	getMe(@Req() req: Request) {
-		return req.user;
+	getMe(@GetUser() user: User) {
+		return user;
+	}
+
+	@Get()
+	getAllUsers() {
+		return this.prisma.user.findMany();
 	}
 }
