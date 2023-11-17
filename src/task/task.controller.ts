9caf97ae01e84e13cecce728cdbf70b008ce5f72
gavryk <gvozdyk23@gmail.com>
2023-11-17 21:34:31 +0200

    added: update task function

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { JwtGuard } from 'src/auth/guard';
+import { TaskStatus } from '@prisma/client';
 
 @Controller('tasks')
 export class TaskController {
@@ -15,5 +16,11 @@ export class TaskController {
 	@Get('/:userId')
 	async getUserTasks(@Param('userId') userId: string) {
 		return this.taskService.getUserTasks(userId);
+	}
+
+	@UseGuards(JwtGuard)
+	@Put(':taskId/status')
+	async updateTaskStatus(@Param('taskId') taskId: string, @Body('status') newStatus: TaskStatus) {
+		return this.taskService.updateTaskStatus(taskId, newStatus);
 	}
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { TaskStatus } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -16,5 +17,23 @@ export class TaskService {
 				userId: userId,
 			},
 		});
+	}
+
+	async updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+		try {
+			const validStatusValues = ['NEW', 'IN_PROGRESS', 'PENDING', 'DONE'];
+			if (!validStatusValues.includes(newStatus)) {
+				throw new Error('Invalid task status');
+			}
+			const updatedTask = await this.prisma.task.update({
+				where: { id: taskId },
+				data: { status: newStatus },
+				select: { id: true, title: true, description: true, status: true },
+			});
+			return updatedTask;
+		} catch (error) {
+			console.error(error);
+			throw new Error('Failed to update task status');
+		}
 	}
 }
