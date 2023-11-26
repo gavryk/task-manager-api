f1bb8f0473e5f17f2d0163d3de9ffa1a3e20c46b
gavryk <gvozdyk23@gmail.com>
2023-11-27 01:42:00 +0200

    added: client url for cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 	app.setGlobalPrefix('api');
-	app.enableCors();
+	app.enableCors({
+		origin: process.env.CLIENT_URL,
+		credentials: true,
+	});
 	await app.listen(3333);
 }
 bootstrap();
